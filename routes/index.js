7d6d8dbb0bc439389be8cd22d5033b7272fb858c
OMmienu <nkedzi@gmail.com>
2023-09-05 15:57:33 -0700

    error fixed

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-import { Router } from 'express';
-import AppController from '../controllers/AppController';
+import AppContoller from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-import UsersController from '../controllers/UsersController';
 
-const router = Router();
+const express = require('express');
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
+const router = express.Router();
+
+router.get('/status', AppContoller.getStatus);
+router.get('/stats', AppContoller.getStats);
+router.post('/users', UserController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
+router.get('/users/me', UserController.getMe);
 router.post('/files', FilesController.postUpload);
-router.get('/files', FilesController.getIndex);
 router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
